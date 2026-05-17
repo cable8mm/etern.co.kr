80b6bb98c66c1737359e844ff007697e1d0aa5c8
Samgu Lee <cable8mm@gmail.com>
2026-05-17 18:34:48 +0900

    Set development environment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      host: true, // 👈 국핵심: Nginx(Herd)가 이 서버를 찾을 수 있게 문을 열어둡니다.
+      port: parseInt(env.VITE_PORT) || 5173,
+      strictPort: true,
+      allowedHosts: true,
+    },
+  };
 });
