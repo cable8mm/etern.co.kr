91d44c2cbd219e4f427a82a8fdcf8561d95f5077
Samgu Lee <cable8mm@gmail.com>
2026-05-18 10:49:28 +0900

    Fix a path error for a custom domain (#3)

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,9 @@ export default defineConfig(({ command }) => {
 
     // 1. 배포(빌드)할 때만 GitHub Pages 용 베이스 경로('/`레포이름`/')를 적용하고,
     //    개발(serve) 때는 루트('/')를 바라보게 합니다.
-    base: isBuild ? '/etern.co.kr/' : '/',
+    // base: isBuild ? '/etern.co.kr/' : '/',
+    // 2. custom domain을 사용한다면 빌드할 때도 루트('/')를 바라보게 합니다.
+    base: '/',
 
     // 2. 개발 서버 설정은 로컬 개발(serve) 때만 활성화되도록 합니다.
     //    빌드할 때는 이 프록시/호스트 설정들이 배포 결과물에 영향을 주지 않습니다.
